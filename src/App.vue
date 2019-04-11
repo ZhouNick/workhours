2a414c8428f7b44ceedb4af4216208c53f490c9e
zhoujiawen <zhoujiawen@cetc.com.cn>
2019-04-11 15:54:03 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,11 @@ export default {
 }
 </script>
 
+<style lang="less">
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
 <style lang="less" src="assets/less/site.less"></style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="login">login</div>
+  <div class="page-login">
+    <div class="login-warp">
+      <mt-field label="帐号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
+      <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+    </div>
+  </div>
 </template>
 
 <script>
