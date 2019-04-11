5c4c215fe750cdb408f850e3903fa8fd92cd8827
jimmy <jimmy.zejia@gmail.com>
2019-04-11 15:53:01 +0800

    修改接口

--- src/pages/work-hours-list/index.vue
+++ src/pages/work-hours-list/index.vue
@@ -1,11 +1,33 @@
 <template>
+<div class='container'>
   <div class="work-hours-list">工时列表</div>
+  <ul>
+    <li v-for="(item , index) in workingHourList" :key='index'>
+      {{item.createtime}}
+      {{item.workinghour}}
+    </li>
+  </ul>
+</div>
 
 </template>
 
 <script>
-// import api from '../../api'
+import api from '@/api'
 export default {
-  name: 'work-hours-list'
+  name: 'work-hours-list',
+  data () {
+    return {
+      workingHourList: []
+    }
+  },
+  mounted () {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData () {
+      const list = await api.workinghourapi({uid: 1})
+      this.workingHourList = list.data.workingHourList
+    }
+  }
 }
 </script>
