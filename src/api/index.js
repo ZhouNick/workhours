b8369347026f604730bd42da677d7153519b856c
zhoujiawen <zhoujiawen@cetc.com.cn>
2019-04-11 15:01:14 +0800

    新增Mint ui

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+let api = axios.create({
+  baseURL: 'http://117.48.208.154:8181/'
+})
+
+export default {
+  login: function (query) {
+    return api.get('userapi/login', { params: query })
+  },
+  loadProjects: function () {
+    return api.get('projectapi/list')
+  }
+}
