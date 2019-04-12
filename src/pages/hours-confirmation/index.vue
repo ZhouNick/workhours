485437474ab8291f92f36e9502e9d39c044c5a32
zhoujiawen <zhoujiawen@cetc.com.cn>
2019-04-12 09:25:49 +0800

    修改登录页样式

--- src/pages/hours-confirmation/index.vue
+++ src/pages/hours-confirmation/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="page-hours-confirmation">
-    
+    <group>
+      <selector title="title"></selector>
+    </group>
   </div>
 </template>
+
+<script>
+import { Group, XInput, XButton } from 'vux'
+export default {
+  name: 'page-hours-confirmation',
+  components: {
+    Group,
+    XInput,
+    XButton
+  },
+  data () {
+    return {
+      project: []
+    }
+  }
+}
+</script>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -2,26 +2,46 @@
   <div class="page-login">
     <div class="login-warp">
       <figure class="logo"></figure>
-      <mt-field label="帐号" placeholder="请输入手机号" type="tel" v-model="query.telephone">
-        <img/>
-      </mt-field>
-      <mt-field label="密码" placeholder="请输入密码" type="password" v-model="query.pwd"></mt-field>
+      <group>
+        <x-input title="帐号" type="tel" is-type="china-mobile" v-model="query.telephone"></x-input>
+        <x-input title="密码" type="password" v-model="query.pwd"></x-input>
+      </group>
       <div class="login-btn">
-        <mt-button type="primary">登录</mt-button>
+        <x-button type="primary" @click="login">登录</x-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Group, XInput, XButton } from 'vux'
+import api from '@/api'
 export default {
   name: 'login',
+  components: {
+    Group,
+    XInput,
+    XButton
+  },
   data () {
     return {
       query: {
         telephone: '',
         pwd: ''
       }
+    }
+  },
+  created() {
+    console.log(this.$toast)
+  },
+  methods: {
+    login () {
+      if (!this.query.telephone) {
+        return
+      }
+      api.login(this.query).then(rsp => {
+        this.$router.push('/')
+      })
     }
   }
 }
